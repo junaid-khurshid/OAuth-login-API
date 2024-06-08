1ab0122907a2c50536967a299746b3274406c348
DESKTOP-FSTC1AE\Juniation <junaid.khursheed@indusvalley.io>
2024-06-08 10:56:42 +0500

    user and login model created

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace OAuth_login_API.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public List<string> Regions { get; set; }
+    }
+
+}
